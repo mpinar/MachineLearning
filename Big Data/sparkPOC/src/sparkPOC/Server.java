42e33a808992a773eb98fe087291194b45944e8f
Mert PINAR <mertpinar@me.com>
2017-12-16 23:35:37 +0000

    sparky

---
+++ Big Data/sparkPOC/src/sparkPOC/Server.java
@@ -0,0 +1,12 @@
+package sparkPOC;
+
+import spark.Spark;
+import spark.debug.DebugScreen;
+
+public class Server {
+	public static void main(String[] args) {
+		Spark.port(9000);
+		DebugScreen.enableDebugScreen();
+	}
+
+}
